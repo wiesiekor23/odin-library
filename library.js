6fc227ae7bc63b86e47ce83929820bf6a5553664
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2025-01-26 15:59:45 +0000

    Create Book adding logic

--- library.js
+++ library.js
@@ -1,1 +1,18 @@
-alert("Hello World!");
+const myLibrary = [];
+
+function Book(title) {
+    this.title = title
+};
+
+function addBookToLibrary(book) {
+    const newBook = new Book(book);
+
+    myLibrary.push(newBook);
+};
+
+addBookToLibrary("Harry Potter");
+addBookToLibrary("The Silence of the Lambs");
+addBookToLibrary("The Hobbit");
+addBookToLibrary("Salem");
+
+console.log(myLibrary);
