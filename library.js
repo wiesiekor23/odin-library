e76730360940eeb2097b3a7f79191bf68047f4f9
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2025-01-30 23:43:54 +0000

    Change some names, small refactor

--- library.js
+++ library.js
@@ -11,37 +11,30 @@ function addBookToLibrary(book, read) {
     myLibrary.push(newBook);
 };
 
-addBookToLibrary("Harry Potter", "Not Read");
-addBookToLibrary("The Silence of the Lambs", "Read");
-addBookToLibrary("The Hobbit", "Not Read");
-addBookToLibrary("Salem", "Read");
-
-console.log(myLibrary);
-
 function displayBooks(array) {
     const container = document.querySelector("#library-container");
     container.innerHTML = "";
     
     array.forEach(element => {
-        const myDiv = document.createElement("div");
-        const myBtn = document.createElement("button");
-        const myChangeReadStatusBtn = document.createElement("button");
-        myDiv.textContent = element.title + " " + element.read;
-        myBtn.textContent = "Remove Book";
-        myChangeReadStatusBtn.textContent = "Change Read Status";
-        myDiv.classList.add("book");
-        myBtn.classList.add("remove-button");
-        myChangeReadStatusBtn.classList.add("change-read-status");
+        const bookDiv = document.createElement("div");
+        const removeBtn = document.createElement("button");
+        const ChangeReadStatusBtn = document.createElement("button");
         
+        bookDiv.textContent = element.title + " " + element.read;
+        removeBtn.textContent = "Remove Book";
+        ChangeReadStatusBtn.textContent = "Change Read Status";
+
+        bookDiv.classList.add("book");
+        removeBtn.classList.add("remove-button");
+        ChangeReadStatusBtn.classList.add("change-read-status");
         
-        container.appendChild(myDiv);
-        myDiv.appendChild(myBtn);
-        myDiv.appendChild(myChangeReadStatusBtn);
+        container.appendChild(bookDiv);
+        bookDiv.appendChild(removeBtn);
+        bookDiv.appendChild(ChangeReadStatusBtn);
     });
     
     removeBook();
     changeStatus();
-    
 }
 
 function removeBook() {
@@ -59,7 +52,6 @@ function removeBookFromArray(array, index) {
     array.splice(index, 1);
 }
 
-
 function changeStatus() {
     const changeReadStatusBtn = document.querySelectorAll(".change-read-status");
     
@@ -71,7 +63,6 @@ function changeStatus() {
     });
 };
 
-
 Book.prototype.changeReadStatus = function() {
     if (this.read == "Read") {
         this.read = "Not Read";
@@ -82,35 +73,28 @@ Book.prototype.changeReadStatus = function() {
 
 /* Add dialog / Modal */
 
-const modalBtn = document.querySelector(".add-button");
-const dialog = document.querySelector(".myDialog");
-const myInput = document.querySelector("input");
-const mySelect = document.querySelector("select");
-const confirmBtn = document.querySelector(".confirm-button");
+function addModal() {
+    const modalBtn = document.querySelector(".add-button");
+    const dialog = document.querySelector(".myDialog");
+    const myInput = document.querySelector("input");
+    const mySelect = document.querySelector("select");
+    const confirmBtn = document.querySelector(".confirm-button");
+    
+    modalBtn.addEventListener("click", () => {
+        dialog.showModal();
+    })
+    
+    confirmBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        dialog.close(addBookToLibrary(myInput.value, mySelect.value));
+        displayBooks(myLibrary);
+    })
+};
 
-modalBtn.addEventListener("click", () => {
-    dialog.showModal();
-})
-
-confirmBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    dialog.close(addBookToLibrary(myInput.value, mySelect.value));
-    displayBooks(myLibrary);
-})
-
-
-function removeAllBooks() {
-    const removeBooks = document.querySelectorAll(".book");
-    removeBooks.forEach(element => {
-        element.remove();
-    });
-}
-
-
-
-const book1 = new Book("Hyperion", "Read");
-book1.changeReadStatus();
-console.log(book1);
-
+addBookToLibrary("Harry Potter", "Not Read");
+addBookToLibrary("The Silence of the Lambs", "Read");
+addBookToLibrary("The Hobbit", "Not Read");
+addBookToLibrary("Salem", "Read");
 
 displayBooks(myLibrary);
+addModal();
